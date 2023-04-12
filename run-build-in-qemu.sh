eb13c41c713dbc8918952f527586e9ef629c2f28
tobozo <tobozo@users.noreply.github.com>
2023-04-12 14:14:45 +0200

    more debug, less code

--- run-build-in-qemu.sh
+++ run-build-in-qemu.sh
@@ -20,76 +20,39 @@ else
   function _debug { return; }
 fi
 
-
 function exit_with_error { echo $1; exit 1; }
-
 
 echo "[INFO] Validating tools"
 
-if [[ ! -f "$ESPTOOL_PY" ]]; then
-  exit_with_error "esptool.py is missing"
-fi
-
-if [[ ! -f "$QEMU_BIN" ]]; then
-  exit_with_error "qemu-system-xtensa is missing"
-fi
+[[ ! -f "$ESPTOOL_PY" ]] && exit_with_error "esptool.py is missing"
+[[ ! -f "$QEMU_BIN" ]] && exit_with_error "qemu-system-xtensa is missing"
 
 echo "[INFO] Validating input data"
 
-if [[ "$ENV_BUILD_FOLDER" == "" ]]; then
-  exit_with_error "No build folder provided"
-fi
-
-if [[ ! -d "$ENV_BUILD_FOLDER" ]]; then
-  exit_with_error "No build folder found, aborting"
-fi
-
-if [[ ! -f "$ENV_BUILD_FOLDER/$ENV_PARTITIONS_CSV" ]]; then
-  exit_with_error "Missing partitions.csv file in build folder"
-fi
+[[ "$ENV_BUILD_FOLDER" == "" ]] && exit_with_error "No build folder provided"
+[[ ! -d "$ENV_BUILD_FOLDER" ]] && exit_with_error "No build folder found, aborting"
+[[ ! -f "$ENV_BUILD_FOLDER/$ENV_PARTITIONS_CSV" ]] && exit_with_error "Missing partitions.csv file in build folder"
 
 if [[ ! -f "$ENV_BUILD_FOLDER/$ENV_OTADATA_BIN" ]]; then
   echo "[INFO] Fetching a copy of boot_app0.bin from espressif/arduino-esp32 repository"
   wget -q https://github.com/espressif/arduino-esp32/raw/master/tools/partitions/boot_app0.bin -O "$ENV_BUILD_FOLDER/$ENV_OTADATA_BIN"
 fi
 
-if [[ ! -f "$ENV_BUILD_FOLDER/$ENV_FIRMWARE_BIN" ]]; then
-  exit_with_error "Missing app0, check your 'firmware-bin' path"
-fi
-
-if [[ ! -f "$ENV_BUILD_FOLDER/$ENV_PARTITIONS_BIN" ]]; then
-  # TODO: generate partitions.bin from partitions.csv using gen_esp32part.py
-  exit_with_error "Missing partitions.bin, check your 'partitions-bin' path"
-fi
-
-if [[ ! -f "$ENV_BUILD_FOLDER/$ENV_BOOTLOADER_BIN" ]]; then
-  exit_with_error "Missing bootloader.bin, check your 'bootloader-bin' path"
-fi
+[[ ! -f "$ENV_BUILD_FOLDER/$ENV_FIRMWARE_BIN" ]] && exit_with_error "Missing app0, check your 'firmware-bin' path"
+# TODO: generate partitions.bin from partitions.csv using gen_esp32part.py
+[[ ! -f "$ENV_BUILD_FOLDER/$ENV_PARTITIONS_BIN" ]] && exit_with_error "Missing partitions.bin, check your 'partitions-bin' path"
+[[ ! -f "$ENV_BUILD_FOLDER/$ENV_BOOTLOADER_BIN" ]] && exit_with_error "Missing bootloader.bin, check your 'bootloader-bin' path"
 
 if [[ ! -f "$ENV_BUILD_FOLDER/$ENV_SPIFFS_BIN" ]]; then
   echo "[INFO] Creating empty SPIFFS file"
   touch "$ENV_BUILD_FOLDER/$ENV_SPIFFS_BIN"
 fi
 
-if [[ "$ENV_FLASH_SIZE" =~ '^(2|4|8|16)$' ]]; then
-  exit_with_error "Invalid flash size (valid values=2,4,8,16)"
-fi
-
-if [[ "$ENV_QEMU_TIMEOUT" =~ '^[0-9]{1,3}$' ]]; then
-  exit_with_error "Invalid timeout value (valid values=0...999)"
-fi
-
-if [[ "$ENV_BOOTLOADER_ADDR" =~ '^0x[0-9a-z-A-Z]{1,8}$' ]]; then
-  exit_with_error "Invalid bootloader address (valid values=0x0000...0xffffffff)"
-fi
-
-if [[ "$ENV_PARTITIONS_ADDR" =~ '^0x[0-9a-z-A-Z]{1,8}$' ]]; then
-  exit_with_error "Invalid bootloader address (valid values=0x0000...0xffffffff)"
-fi
-
-if [[ "$ENV_PARTITIONS_ADDR" =~ '^0x[0-9a-z-A-Z]{1,8}$' ]]; then
-  exit_with_error "Invalid bootloader address (valid values=0x0000...0xffffffff)"
-fi
+[[ "$ENV_FLASH_SIZE" =~ '^(2|4|8|16)$' ]] && exit_with_error "Invalid flash size (valid values=2,4,8,16)"
+[[ "$ENV_QEMU_TIMEOUT" =~ '^[0-9]{1,3}$' ]] && exit_with_error "Invalid timeout value (valid values=0...999)"
+[[ "$ENV_BOOTLOADER_ADDR" =~ '^0x[0-9a-z-A-Z]{1,8}$' ]] && exit_with_error "Invalid bootloader address (valid values=0x0000...0xffffffff)"
+[[ "$ENV_PARTITIONS_ADDR" =~ '^0x[0-9a-z-A-Z]{1,8}$' ]] && exit_with_error "Invalid bootloader address (valid values=0x0000...0xffffffff)"
+[[ "$ENV_PARTITIONS_ADDR" =~ '^0x[0-9a-z-A-Z]{1,8}$' ]] && exit_with_error "Invalid bootloader address (valid values=0x0000...0xffffffff)"
 
 echo "[INFO] Extracting partitions info from $ENV_BUILD_FOLDER/$ENV_PARTITIONS_CSV"
 
@@ -103,9 +66,9 @@ for index in "${!rows[@]}"
 do
   IFS=',' read -ra csv_columns <<< "${rows[$index]}" # split columns
   case "${csv_columns[0]}" in
-    otadata)  OTADATA_ADDR="${csv_columns[3]}" ;;
-    app0)     FIRMWARE_ADDR="${csv_columns[3]}"  ;;
-    spiffs)   SPIFFS_ADDR="${csv_columns[3]}"    ;;
+    otadata)  OTADATA_ADDR="${csv_columns[3]}"  _debug "OTADATA_ADDR=$OTADATA_ADDR";;
+    app0)     FIRMWARE_ADDR="${csv_columns[3]}" _debug "FIRMWARE_ADDR=$FIRMWARE_ADDR";;
+    spiffs)   SPIFFS_ADDR="${csv_columns[3]}"   _debug "SPIFFS_ADDR=$SPIFFS_ADDR";;
     *) _debug "Ignoring ${csv_columns[0]}:${csv_columns[3]}" ;;
   esac
 done
