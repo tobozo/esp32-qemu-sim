02d1ec2bf73bf8b9da3162ebe4b7402dad8d90f1
tobozo <tobozo@users.noreply.github.com>
2023-04-13 17:08:48 +0200

    enabled psram

--- test/test.ino
+++ test/test.ino
@@ -3,6 +3,9 @@ void setup()
 {
   Serial.begin( 115200 );
   Serial.println("Hello World!");
+  if( psramInit() ) {
+    Serial.printf("Found %d bytes of PSRAM!\n", ESP.getFreePsram() );
+  }
 }
 
 void loop()
