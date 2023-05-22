6b723ccb704fe446466f21e502b0c4ab9d25c7dc
tobozo <tobozo@users.noreply.github.com>
2023-05-22 16:17:16 +0200

    wait for serial

--- test/test.ino
+++ test/test.ino
@@ -2,6 +2,7 @@
 void setup()
 {
   Serial.begin( 115200 );
+  while(!Serial.available()); // wait for serial to come up
   Serial.println("Hello World!");
   if( psramInit() ) {
     Serial.printf("Found %d bytes of PSRAM!\n", ESP.getFreePsram() );
