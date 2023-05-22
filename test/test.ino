3b45a346015c1ad63f23401a754a5b4fba5a35c3
tobozo <tobozo@users.noreply.github.com>
2023-05-22 16:27:26 +0200

    wut?

--- test/test.ino
+++ test/test.ino
@@ -4,13 +4,15 @@ void setup()
   Serial.begin( 115200 );
   while(!Serial.available()); // wait for serial to come up
   Serial.println("Hello World!");
-  if( psramInit() ) {
-    Serial.printf("Found %d bytes of PSRAM!\n", ESP.getFreePsram() );
-  } else {
-    Serial.println("This device has no PSRAM" );
-  }
+  // if( psramInit() ) {
+  //   Serial.printf("Found %d bytes of PSRAM!\n", ESP.getFreePsram() );
+  // } else {
+  //   Serial.println("This device has no PSRAM" );
+  // }
 }
 
 void loop()
 {
+  Serial.println("Hello World!");
+  delay(1000);
 }
